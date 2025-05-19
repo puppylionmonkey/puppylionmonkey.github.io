920905b06836cf0f374a0b84eb80a7ee4efd577c
Andrew <puppylionmonkey33@gmail.com>
2025-05-19 13:53:54 +0800

    new v

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2052081642",
+        serviceWorkerVersion: "181851585",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const appRunner = await engineInitializer.initializeEngine({useColorEmoji: useColorEmoji});

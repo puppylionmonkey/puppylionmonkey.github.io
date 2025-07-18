0ce72283706b4e4d7f20a474fee0edf470d4a43d
puppylionmonkey <puppylionmonkey33@gmail.com>
2025-07-19 07:18:26 +0800

    加上有頭鹿作品

---
+++ deer_test/static/js/index.js
@@ -0,0 +1,10 @@
+// new fullpage('#fullpage', {
+//     autoScrolling: true,
+//     navigation: false,
+//     navigationPosition: 'right',
+//     navigationTooltips: false,
+//     showActiveTooltip: false,
+//     scrollOverflow: true,
+//     sectionSelector: '.vertical-scrolling',
+//    normalScrollElements: '#scrolldiv'
+// });
